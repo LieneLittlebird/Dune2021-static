4fdeb937ac977885bd2ca1a6937c98e23012d6df
LieneLittlebird <liene.putnina20@gmail.com>
2021-08-14 13:47:25 +0300

    fix: deleted console.log on Register

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -35,7 +35,6 @@ const Register = () => (
     })}
     onSubmit={(values, { setSubmitting }) => {
       setTimeout(() => {
-        // console.log(JSON.stringify(values, null, 2));
         setSubmitting(false);
         document.location.href = "/";
       }, 400);
